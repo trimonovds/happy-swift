63348b56c6368f5697797f9be76ae9a59ce2c707
GitHub Action <action@github.com>
2022-11-04 21:02:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/trimonovds/happyswift/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/trimonovds/happyswift/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.trimonovds.happyswift.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.trimonovds.happyswift.MyBundle
 
 class MyApplicationService {
 
